7048c945eb333904e166b7ea08b66bfd6ca64daa
RSimer <65471088+RSimer@users.noreply.github.com>
2021-05-20 10:22:45 -0700

    script.js

--- script.js
+++ script.js
@@ -7,15 +7,40 @@ var uppercase = ["A","B","C","D","E","F","G",'H',"I","J","K","L","M","N","O","P"
 
 var special = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","{","}","[","]",";",":","'",",",".","/","<",">","?","`","~","1","2","3","4","5","7","8","9","0"]
 
+var putIn = [];
 
-characters = characters.concat(lowercase);
-characters = characters.concat(uppercase);
-characters = characters.concat(special);
+// characters = characters.concat(lowercase);
+// characters = characters.concat(uppercase);
+// characters = characters.concat(special);
 
-function confirmCharacters(){
+function generatePassword(){
 
-  confirm("do you want lowercase letters")
-  confirm(lowercase)
+ var addLowercase = ("Do you want lowercase letters?");
+ console.log(addLowercase);
+var addUppercase = ("do you want uppercase letters?");
+console.log(addUppercase);
+var addSpecial = ("Do you want special characters and/or numbers because I was too lazy to put numbers in their own category?");
+console.log(addSpecial);
+var passwordLength = "The password's gotta be inbetween 8 and 128 characters. Why someone would create a 128 character password is beyond me."
+while(passwordLength < 8 || passwordLength > 128){
+alert ("gotta read homeboi,");
+passwordLength = prompt("The password's gotta be inbetween 8 and 128 characters. Why someone would create a 128 character password is beyond me.")
+}
+console.log(passwordLength);
+
+if (addLowercase){
+  putIn.push(lowercase);
+}
+console.log(putIn);
+if (addUppercase){
+  putIn.push(uppercase);
+}
+console.log(putIn);
+if (addSpecial){
+  putIn.push(special);
+}
+console.log(putIn);
+
 }
 
 // Write password to the #password input
@@ -33,15 +58,5 @@ generateBtn.addEventListener("click", writePassword);
 
 // create a function that on click will prompt the character length for the password (must be between 8-128 characters) 
 
-// must create the variable for every character 'a','b' 'A','B'
-
 // after prompt function add confirm function for adding capital, undercase and special characters
-if (condition) {
-  
- characterLength 
-
-   minLength = 8;
-   maxLength = 128;
-
- }
 
